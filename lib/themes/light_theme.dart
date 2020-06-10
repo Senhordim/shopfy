8865dfb890a4bb5fa419e7adfa7d39a52c331c5b
Diego Collares <diego.collares@gmail.com>
2020-06-09 23:25:06 -0300

    add themes

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const brightness = Brightness.light;
+const primaryColor = const Color(0xff00C569);
+const lightColor = const Color(0xffffffff);
+const backgroundColor = const Color(0xffF5F5F5);
+
+ThemeData lightTheme() {
+  return ThemeData(
+    brightness: brightness,
+  );
+}
